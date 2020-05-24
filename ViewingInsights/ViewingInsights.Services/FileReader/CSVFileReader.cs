67d1e15a5ceda9aa5fefb9d0e1eee0b6dbeea1e7
nunor <Nuno.Ramanlal@derivco.co.uk>
2020-05-24 14:52:43 +0100

    fileReader service added

---
+++ ViewingInsights/ViewingInsights.Services/FileReader/CSVFileReader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ViewingInsights.Services.FileReader
+{
+    public class CSVFileReader
+    {
+        public void ReadFile(string fileName)
+        {
+            StreamReader reader = new StreamReader(fileName);
+            reader.ReadLine();
+            Dictionary<string, string> viewingData = new Dictionary<string, string>();
+
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                string[] values = line.Split(',');
+                viewingData[values[0]] = values[1];
+            }
+
+            foreach (KeyValuePair<string, string> kvp in viewingData)
+            {
+                Console.WriteLine(kvp.Key + " " + kvp.Value);
+            }
+
+        }
+    }
+}
---
+++ ViewingInsights/ViewingInsights.Services/Program.cs
@@ -0,0 +1,13 @@
+﻿using ViewingInsights.Services.FileReader;
+
+namespace ViewingInsights.Services
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CSVFileReader csv = new CSVFileReader();
+            csv.ReadFile(@"C:\Users\nunor\test1.csv"); 
+        }
+    }
+}
