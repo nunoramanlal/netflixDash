62dbc74e5221355fc5519864399be45eb565662c
nunor <Nuno.Ramanlal@derivco.co.uk>
2020-05-25 10:40:00 +0100

    form added; form model

---
+++ ViewingInsights/ViewingInsights.App/Data/ExampleModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ViewingInsights.App.Data
+{
+    public class ExampleModel
+    {
+        [Required]
+        [StringLength(10, ErrorMessage = "Name is too long.")]
+        public string Name { get; set; }
+    }
+}
