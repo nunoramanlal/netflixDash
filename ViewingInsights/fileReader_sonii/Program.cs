efb3ef718f44f88e3ecd1cc1d878c07cbb317435
Sonica Samgi <sonica_samgi@hotmail.com>
2020-05-27 22:05:01 +0100

    filereader_s

---
+++ ViewingInsights/fileReader_sonii/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace fileReader_sonii
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            StreamReader x;
+            Console.WriteLine("Hello World!"); 
+            string path= @"C:\Users\Sonica Samgi\Documents\teste.txt";
+
+            x = File.OpenText(path);
+            string linha = x.ReadLine();
+            Console.WriteLine(linha);
+            Console.WriteLine("o nuno é estupido");
+
+        }
+    }
+}
